22318af27a70a4d87793881cfa887c0cbc1ff551
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-03-12 01:38:12 +0700

    Add: getSex function ; Update: định dạng in

--- Code/Program/RoomRenter.hpp
+++ Code/Program/RoomRenter.hpp
@@ -21,8 +21,10 @@ struct RoomRenter
 bool validDate(int *day, int *month, int *year);
 void inputDate(int *day, int *month, int *year);
 bool chooseSex();
+const char *getSex(bool sex);
 void inputARoomRenter(RoomRenter *roomRenter);
-void outputARoomRenter(RoomRenter roomRenter);
+void outputARoomRenterByVertical(RoomRenter roomRenter);   // in theo chiều dọc
+void outputARoomRenterByHorizontal(RoomRenter roomRenter); // in theo chiều ngang - có định dạng in
 
 bool validDate(int *day, int *month, int *year) // kiểm tra ngày nhập
 {
@@ -63,7 +65,7 @@ void inputDate(int *day, int *month, int *year)
         scanf("%d", year);
         if (!validDate(day, month, year))
         {
-            printf("\n\t(!) Ngay thang khong hop le - Nhap lai (!)\n");
+            printf("\n\t(!) Ngay sinh khong hop le - Nhap lai (!)\n\a");
         }
     } while (!validDate(day, month, year));
 }
@@ -89,54 +91,65 @@ bool chooseSex()
             exit = true;
             break;
         default:
-            printf("\n\t(!) Gioi tinh khong hop le - Nhap lai (!)\n");
+            printf("\n\t(!) Gioi tinh khong hop le - Nhap lai (!)\n\a");
             break;
         }
     } while (!exit);
     return sex;
 }
 
+const char *getSex(bool sex)
+{
+    if (sex == 0)
+        return "Nu";
+    else
+        return "Nam";
+}
+
 void inputARoomRenter(RoomRenter *roomRenter)
 {
-    printf("\n(?) Nhap ho va ten: ");
+    system("cls");
+    printf("(?) Nhap ho va ten: ");
     gets(roomRenter->fullName);
     system("cls");
-    printf("\n\tNgay sinh\n");
+    printf("\tNgay sinh\n");
     inputDate(&roomRenter->birthDay.day, &roomRenter->birthDay.month, &roomRenter->birthDay.year);
     system("cls");
-    printf("\n(?) Gioi tinh");
+    printf("(?) Gioi tinh");
     roomRenter->sex = chooseSex();
     system("cls");
     fflush(stdin);
     do
     {
-        printf("\n(?) Nhap so dien thoai: ");
+        printf("(?) Nhap so dien thoai: ");
         gets(roomRenter->phoneNumber);
         if (strlen(roomRenter->phoneNumber) <= 0 || strlen(roomRenter->phoneNumber) > 10 || strlen(roomRenter->phoneNumber) != 10)
-            printf("\n\t(!) So dien thoai khong hop le - Nhap lai (!)\n");
+            printf("\n\t(!) So dien thoai khong hop le - Nhap lai (!)\n\a");
     } while (strlen(roomRenter->phoneNumber) <= 0 || strlen(roomRenter->phoneNumber) > 10 || strlen(roomRenter->phoneNumber) != 10);
     system("cls");
     do
     {
-        printf("\n(?) Nhap so CCCD: ");
+        printf("(?) Nhap so CCCD: ");
         gets(roomRenter->identityCard);
         if ((strlen(roomRenter->identityCard) <= 0 || strlen(roomRenter->identityCard) > 12) || strlen(roomRenter->identityCard) != 12)
-            printf("\n\t(!) So CMND/CCCD khong hop le - Nhap lai (!)\n");
+            printf("\n\t(!) So CMND/CCCD khong hop le - Nhap lai (!)\n\a");
     } while ((strlen(roomRenter->identityCard) <= 0 || strlen(roomRenter->identityCard) > 12) || strlen(roomRenter->identityCard) != 12);
     system("cls");
 }
 
-void outputARoomRenter(RoomRenter roomRenter)
+void outputARoomRenterByVertical(RoomRenter roomRenter)
 {
     printf("\nHo va ten: %s", roomRenter.fullName);
     printf("\nNgay sinh: %d/%d/%d", roomRenter.birthDay.day, roomRenter.birthDay.month, roomRenter.birthDay.year);
     char sex[4];
-    if (roomRenter.sex == 0)
-        strcpy(sex, "Nu");
-    else
-        strcpy(sex, "Nam");
-    printf("\nGioi tinh: %s", sex);
+
+    printf("\nGioi tinh: %s", getSex(roomRenter.sex));
     printf("\nSo dien thoai: %s", roomRenter.phoneNumber);
     printf("\nSo CCCD: %s", roomRenter.identityCard);
-    printf("\nSo phong: %d", roomRenter.roomNumber);
+    printf("\nSo phong: %d\n", roomRenter.roomNumber);
+}
+
+void outputARoomRenterByHorizontal(RoomRenter roomRenter)
+{
+    printf("| %-30s | %-.2d/%.-2d/%-.4d   | %-9s | %-15s | %-20s | %-10d |\n", roomRenter.fullName, roomRenter.birthDay.day, roomRenter.birthDay.month, roomRenter.birthDay.year, getSex(roomRenter.sex), roomRenter.phoneNumber, roomRenter.identityCard, roomRenter.roomNumber);
 }
