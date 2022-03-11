825b6818c68b4e28b15c941f0e4e60b056d13245
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-03-12 02:06:28 +0700

    Add function: add a node, add a room renter, output list

--- Code/Program/RoomRenters.hpp
+++ Code/Program/RoomRenters.hpp
@@ -15,6 +15,9 @@ struct RoomRenters
 
 void createList(RoomRenters *roomRenters);
 Node *createNode(RoomRenter value);
+void addNodeInTail(RoomRenters *roomRenters, RoomRenter roomRenter);
+void addARoomRenter(RoomRenters *roomRenters);
+void outputRoomRenters(RoomRenters roomRenters);
 
 void createList(RoomRenters *roomRenters)
 {
@@ -36,4 +39,41 @@ Node *createNode(RoomRenter value)
         p->pNext = NULL;
     }
     return p;
+}
+
+void addNodeInTail(RoomRenters *roomRenters, RoomRenter roomRenter)
+{
+    Node *p = createNode(roomRenter);
+    if (roomRenters->pHead == NULL)
+        roomRenters->pHead = roomRenters->pTail = p;
+    else
+    {
+        roomRenters->pTail->pNext = p;
+        roomRenters->pTail = p;
+    }
+}
+
+void addARoomRenter(RoomRenters *roomRenters)
+{
+    RoomRenter r;
+    inputARoomRenter(&r);
+    printf("\n\tTHONG TIN NGUOI THUE VUA NHAP\n");
+    outputARoomRenterByVertical(r);
+    system("pause");
+    addNodeInTail(roomRenters, r);
+}
+
+void outputRoomRenters(RoomRenters roomRenters)
+{
+    int count = 1;
+    printf("+ ------- + ----------------------------- + ------------- + --------- + --------------- + -------------------- + ---------- + \n");
+    printf("|   STT   |           Ho va ten           |   Ngay sinh   | Gioi tinh |  So dien thoai  |        So CCCD       |  So phong  | \n");
+    printf("+ ------- + ----------------------------- + ------------- + --------- + --------------- + -------------------- + ---------- + \n");
+
+    for (Node *t = roomRenters.pHead; t != NULL; t = t->pNext)
+    {
+        printf("| %-7d ", count++);
+        outputARoomRenterByHorizontal(t->data);
+    }
+    printf("+ ------- + ----------------------------- + ------------- + --------- + --------------- + -------------------- + ---------- + \n");
 }
