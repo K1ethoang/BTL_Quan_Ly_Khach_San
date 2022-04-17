b4ad23c6830a3a8aa2f10bf6ac53f04997785756
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-04-18 06:11:13 +0700

    Update

--- Code/Program/Room.hpp
+++ Code/Program/Room.hpp
@@ -18,86 +18,86 @@ void removeEnterChar(char *s);             // xoá kí tự \n
 void readARoom(FILE *fileIn, Room &room);  // đọc 1 phòng từ file
 void writeARoom(FILE *fileOut, Room room); // ghi 1 phòng
 
-void chooseRoomType(Room &room)
-{
-    int choose;
-    bool exit = false;
-    do
-    {
-        printf("\n%50c  1. Thuong", ' ');
-        printf("\n%50c  2. Cao cap", ' ');
-        printf("\n%50cNhap lua chon: ", ' ');
-        scanf("%d", &choose);
-        switch (choose)
-        {
-        case 1:
-            strcpy(room.type, "Thuong");
-            exit = true;
-            break;
-        case 2:
-            strcpy(room.type, "Cao cap");
-            exit = true;
-            break;
-        default:
-            printf("\n\t%40c(!) Loai phong khong hop le - Nhap lai (!)\n\a", ' ');
-            break;
-        }
-    } while (!exit);
-}
+// void chooseRoomType(Room &room)
+// {
+//     int choose;
+//     bool exit = false;
+//     do
+//     {
+//         printf("\n%50c  1. Thuong", ' ');
+//         printf("\n%50c  2. Cao cap", ' ');
+//         printf("\n%50cNhap lua chon: ", ' ');
+//         scanf("%d", &choose);
+//         switch (choose)
+//         {
+//         case 1:
+//             strcpy(room.type, "Thuong");
+//             exit = true;
+//             break;
+//         case 2:
+//             strcpy(room.type, "Cao cap");
+//             exit = true;
+//             break;
+//         default:
+//             printf("\n\t%40c(!) Loai phong khong hop le - Nhap lai (!)\n\a", ' ');
+//             break;
+//         }
+//     } while (!exit);
+// }
 
-void chooseRoomStatus(Room &room)
-{
-    int choose;
-    bool exit = false;
-    do
-    {
-        printf("\n%50c  1. Phong trong", ' ');
-        printf("\n%50c  2. Phong day", ' ');
-        printf("\n%50cNhap lua chon: ", ' ');
-        scanf("%d", &choose);
-        switch (choose)
-        {
-        case 1:
-            room.isActive = 0;
-            exit = true;
-            break;
-        case 2:
-            room.isActive = 1;
-            exit = true;
-            break;
-        default:
-            printf("\n\t%40c(!) Tinh trang phong khong hop le (!)\n\a", ' ');
-            break;
-        }
-    } while (!exit);
-}
+// void chooseRoomStatus(Room &room)
+// {
+//     int choose;
+//     bool exit = false;
+//     do
+//     {
+//         printf("\n%50c  1. Phong trong", ' ');
+//         printf("\n%50c  2. Phong day", ' ');
+//         printf("\n%50cNhap lua chon: ", ' ');
+//         scanf("%d", &choose);
+//         switch (choose)
+//         {
+//         case 1:
+//             room.isActive = 0;
+//             exit = true;
+//             break;
+//         case 2:
+//             room.isActive = 1;
+//             exit = true;
+//             break;
+//         default:
+//             printf("\n\t%40c(!) Tinh trang phong khong hop le (!)\n\a", ' ');
+//             break;
+//         }
+//     } while (!exit);
+// }
 
-void inputARoom(Room &room)
-{
-    system("cls");
-    printf("\n%50c(?) Nhap so phong: ", ' ');
-    scanf("%d", &room.number);
-    system("cls");
-    printf("\n%50c(*) Loai phong (*)", ' ');
-    fflush(stdin);
-    chooseRoomType(room);
-    system("cls");
-    printf("\n%50c(*) Tinh trang (*)", ' ');
-    chooseRoomStatus(room);
-}
+// void inputARoom(Room &room)
+// {
+//     system("cls");
+//     printf("\n%50c(?) Nhap so phong: ", ' ');
+//     scanf("%d", &room.number);
+//     system("cls");
+//     printf("\n%50c(*) Loai phong (*)", ' ');
+//     fflush(stdin);
+//     chooseRoomType(room);
+//     system("cls");
+//     printf("\n%50c(*) Tinh trang (*)", ' ');
+//     chooseRoomStatus(room);
+// }
 
-void outputARoomByVertical(Room room)
-{
-    fflush(stdin);
-    printf("\n%50cSo phong: %d", ' ', room.number);
-    printf("\n%50cLoai phong: %s", ' ', room.type);
-    char roomStatus[20];
-    if (room.isActive == 1) // isActive = 1
-        strcpy(roomStatus, "Day");
-    else // isActive = 0
-        strcpy(roomStatus, "Trong");
-    printf("\n%50cTinh trang: %s", ' ', roomStatus);
-}
+// void outputARoomByVertical(Room room)
+// {
+//     fflush(stdin);
+//     printf("\n%50cSo phong: %d", ' ', room.number);
+//     printf("\n%50cLoai phong: %s", ' ', room.type);
+//     char roomStatus[20];
+//     if (room.isActive == 1) // isActive = 1
+//         strcpy(roomStatus, "Day");
+//     else // isActive = 0
+//         strcpy(roomStatus, "Trong");
+//     printf("\n%50cTinh trang: %s", ' ', roomStatus);
+// }
 
 void outputARoomByHorizontal(Room room)
 {
